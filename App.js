7f4eb7af0c21c6f28f7f7304187e59f96b35a1a1
naniprabhakar <55038229+naniprabhakar@users.noreply.github.com>
2019-09-21 16:10:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      <h1>Basic 87</h1>
+      <h6>Free HTML5 Website Template</h6>
+      <div class="heading1">
+                    <ul class="text1">
+                        <li class=" text2">TEXT LINK</li>
+                        <li class=" text2">TEXT LINK</li>
+                        <li class=" text2">TEXT LINK</li>
+                        <li class=" text2">TEXT LINK</li>
+                        <li class=" text2">TEXT LINK</li>
+                    </ul>
+                </div>
+                <div class="box">
+                <div class="box1"><div class="boxing">215*315</div><p>IMAGE CAPTION HERE</p></div>
+                <div class="box1"><div class="boxing">215*315</div><p>IMAGE CAPTION HERE</p></div>
+                <div class="box1"><div class="boxing">215*315</div><p>IMAGE CAPTION HERE</p></div>
+                <div class="box1"><div class="boxing">215*315</div><p>IMAGE CAPTION HERE</p></div>
+            </div>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
